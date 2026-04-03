4180b2fe500f6e45440a8db98ce5c7e5acee31f5
dpepigpt-glitch <dpepigpt@gmail.com>
2026-04-03 14:17:26 -0300

    Atualizar o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,7 @@ const DEFENSORES = {
   "Dra. Andrea Melo de Carvalho": "1ª Defensoria de Família",
   "Dra. Dayana Sampaio Mendes Magalhães": "2ª Defensoria Pública Regional de Altos",
   "Dr. Eric Leonardo Pires de Melo": "7ª Defensoria de Família",
+  "Dr. Marcos Martins de Oliveira": "2ª Defensoria de Floriano",
   "Dra. Priscila Gimenes do Nascimento Godói": "2ª Defensoria Regional de União",
   "Dra. Julyanne Cristine Douglas Leone": "Assessora - 2ª Defensoria Itinerante",
 };
